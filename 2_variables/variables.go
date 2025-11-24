05602fceab64eb6af3631d82aab0aea0343c4494
Muhammad Ismail <m.ismailusman47@gmail.com>
2025-11-25 01:16:13 +0500

    Variables in Go

---
+++ 2_variables/variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var name string = "John" // EXPLICITLY MENTION TYPE
+	var age = 25             // TYPE AUTO INFERRED
+	count := 10              // SHORT DECLARATION (INSIDE FUNCTIONS ONLY)
+
+	// MULTIPLE VARIABLES:
+	var x, y int = 1, 2
+	a, b := "hello", 42
+
+	// CONSTANTS
+	const pi = 3.14
+
+	fmt.Println(name, age, count, x, y, a, b);
+}
+
+// NOTE: UNUSED VARIABLES = COMPILE ERROR (GO DOESN'T TOLERATE DEAD CODE)
