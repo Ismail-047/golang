05a7e60a1a458caa8a6117623df0bc64235da16b
Muhammad Ismail <m.ismailusman47@gmail.com>
2025-11-25 23:26:23 +0500

    Arrays Copy

--- 7_arrays/arrays.go
+++ 7_arrays/arrays.go
@@ -2,25 +2,40 @@ package main
 
 import "fmt"
 
+/*
+	NOTE: WE ALMOST NEVER USE ARRAYS DIRECTLY. IN REAL GO CODE, WE USE SLICES INSTEAD. ARRAYS ARE TOO RIGID - THEIR FIXED SIZE MAKES THEM IMPRACTICAL FOR MOST USE CASES.
+*/
+
 func main() {
 	/*
-		ARRAY -
-		IF NOT ASSIGNED THEN ZEROED VALUE OF THAT DATA TYPE IS ASSIGNED ON EACH INDEX
+		ARRAY - IF NOT INITIALIZED, THEN ZEROED VALUE OF THAT DATA TYPE IS ASSIGNED ON EACH INDEX BY DEFAULT
 	*/
-	var arr [3]int
-	fmt.Println(arr)
-	fmt.Println(len(arr)) // ARRAY LENGTH
 
-	// ARRAY WITH INITIAL VALUES
-	var arr2 [3]int = [3]int{1, 2, 3}
-	fmt.Println(arr2)
+	// DECLARATION
+	var arr [5]int // [0 0 0 0 0]
 
-	// ARRAY WITH INITIAL VALUES
-	arr3 := [3]int{1, 2, 3}
-	fmt.Println(arr3)
+	var names [3]string // ["" "" ""]
+
+	primes := [4]int{2, 3, 5, 7} // DECLARATION & INITIALIZATION
+
+	auto := [...]int{1, 2, 3} // COMPILER COUNTS LENGTH
+
+	// ARRAY LENGTH
+	var length = len(arr)
 
 	// 2D ARRAY
 	arr4 := [2][2]int{{1, 2}, {3, 4}}
 	fmt.Println(arr4) // [[1 2] [3 4]]
+
+	// NOTE: ARRAYS ARE VALUES, NOT REFERENCES. WHEN YOU ASSIGN OR PASS AN ARRAY, GO COPIES THE ENTIRE THING:
+	a := [3]int{1, 2, 3}
+
+	b := a // b IS A COPY
+	b[0] = 99
+
+	fmt.Println(a) // [1 2 3] - UNCHANGED
+	fmt.Println(b) // [99 2 3] - CHANGED
+
+	fmt.Println(arr, names, primes, auto, length)
 
 }
